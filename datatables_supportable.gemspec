2a2d46a21fa9c2295f0b18e988c3408ea370488d
kkzhang <k@z-pk.com>
2014-02-10 02:02:50 +0800

    update

--- datatables_supportable.gemspec
+++ datatables_supportable.gemspec
@@ -6,10 +6,10 @@ require 'datatables_supportable/version'
 Gem::Specification.new do |spec|
   spec.name          = "datatables_supportable"
   spec.version       = DatatablesSupportable::VERSION
-  spec.authors       = ["TODO: Write your name"]
-  spec.email         = ["TODO: Write your email address"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.authors       = ["kkzhang"]
+  spec.email         = [""]
+  spec.summary       = %q{make activerecord support datatables ajax mode}
+  spec.description   = %q{}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
+
+  spec.add_runtime_dependency "activerecord"
+  spec.add_runtime_dependency "actionpack"
 end
--- lib/datatables_supportable.rb
+++ lib/datatables_supportable.rb
@@ -1,5 +1,61 @@
 require "datatables_supportable/version"
 
 module DatatablesSupportable
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  included do
+  end
+
+  module ClassMethods
+
+    attr_accessor :total_count
+
+    def datatables(params, columns)
+
+      self.total_count = self.count
+
+      #filtering
+
+      if (params.has_key? :sSearch) and not params[:sSearch].empty?
+        _cond = ""
+        columns.each_with_index do |c,index|
+          if index+1 == columns.length
+            _cond += "#{c} LIKE ?"
+          else
+            _cond += "#{c} LIKE ? OR "
+          end
+        end
+        @comps = where(_cond, "%#{params[:sSearch]}%","%#{params[:sSearch]}%","%#{params[:sSearch]}%","%#{params[:sSearch]}%")
+      else
+        @comps = self
+      end
+
+
+
+      #ordering
+      if params.has_key? :iSortCol_0
+        params[:iSortingCols].to_i.times do |index|
+          if params["bSortable_#{index}"]=='true'
+            if params.has_key? "sSortDir_#{index}"
+              _order = params["sSortDir_#{index}"]
+              if _order == 'asc'
+                @comps = @comps.order(columns[index])
+              else
+                @comps = @comps.order(columns[index]=>:desc)
+              end
+            end
+          end
+        end
+      end
+
+      # pagination
+      if (params.has_key? :iDisplayStart) and (params.has_key? :iDisplayLength)
+        @comps = @comps.offset(params[:iDisplayStart]).limit(params[:iDisplayLength])
+      end
+
+      @comps
+    end
+  end
 end
+
+ActiveRecord::Base.send :include, DatatablesSupportable
